2b9de5780bdeda6704ba2ecb3e9308ffe5eea000
Reggallucky <66362229+Reggallucky@users.noreply.github.com>
2023-01-14 15:20:35 +0700

    TUgas

---
+++ b/b.java
@@ -0,0 +1,9 @@
+package b;
+
+
+public interface b{
+    void nama();
+    void nama_panjang();
+    void temap_tinggal();
+    void usia();
+}
---
+++ jalan/jalan.java
@@ -0,0 +1,31 @@
+package jalan;
+import b.b;
+
+
+public class jalan implements b{
+
+    @Override
+    public void nama() {
+        String a="akhdan";
+        System.out.println(a);
+    }
+
+    @Override
+    public void nama_panjang() {
+        String b="Fahru Rizqi";
+        System.out.println(b);
+    }
+
+    @Override
+    public void temap_tinggal() {
+        String c="bekasi";
+        System.out.println(c);
+    }
+
+    @Override
+    public void usia() {
+        int d=21;
+        System.out.println(d);
+    }
+    
+}
